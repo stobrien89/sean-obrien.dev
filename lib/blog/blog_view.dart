dc387b4c88318d68497a04edaa0ebaa801093b7f
stobrien89 <stobrien@crimson.ua.edu>
2020-08-19 19:36:09 -0700

    refactor blog cards, format date from rss feed

--- lib/blog/blog_view.dart
+++ lib/blog/blog_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio_v2/utils/hover_extensions.dart';
+
 import 'package:portfolio_v2/components/desktop_view_builder.dart';
 import 'package:portfolio_v2/components/mobile_desktop_view_builder.dart';
 import 'package:portfolio_v2/components/mobile_view_builder.dart';
-import 'package:portfolio_v2/experience/experience_container.dart';
+
 import 'package:provider/provider.dart';
 import 'package:webfeed/domain/rss_feed.dart';
 import 'package:http/http.dart' as http;
+import 'package:webfeed/domain/rss_item.dart';
+
+import 'blog_card.dart';
 
 class BlogView extends StatelessWidget {
   const BlogView({
@@ -16,7 +19,7 @@ class BlogView extends StatelessWidget {
   static const title = 'Blog';
   @override
   Widget build(BuildContext context) {
-    return FutureProvider<RssFeed>(
+    return FutureProvider<List<RssItem>>(
         create: (_) => getArticles(),
         child: MobileDesktopViewBuilder(
           mobileView: BlogMobileView(),
@@ -28,7 +31,7 @@ class BlogView extends StatelessWidget {
 class BlogDesktopView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final articles = context.watch<RssFeed>();
+    final articles = context.watch<List<RssItem>>();
     if (articles == null) return CircularProgressIndicator();
     return DesktopViewBuilder(
       backgroundColor: Color.fromRGBO(46, 184, 155, 1),
@@ -39,9 +42,11 @@ class BlogDesktopView extends StatelessWidget {
         ),
         Row(
           children: [
-            for (var i = 0; i < 2; i++)
+            for (final article in articles)
               Expanded(
-                child: BlogCard(),
+                child: BlogCard(
+                  article: article,
+                ),
               )
           ],
         ),
@@ -53,64 +58,18 @@ class BlogDesktopView extends StatelessWidget {
   }
 }
 
-class BlogCard extends StatelessWidget {
-  const BlogCard({
-    Key key,
-    this.isMobile,
-  }) : super(key: key);
-
-  final bool isMobile;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      margin: EdgeInsets.all(8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: Color.fromRGBO(240, 121, 99, 1),
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(
-              horizontal: 8,
-              vertical: isMobile ?? false ? 20 : 40,
-            ),
-            child: Text(
-              'Testing the Blog App',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline4
-                  .copyWith(color: Colors.white),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text('5 august 2020', style: textStyle(isGray: true)),
-                SizedBox(height: 15),
-                Text('Hot Dogs' * 30, maxLines: 3, style: textStyle())
-              ],
-            ),
-          )
-        ],
-      ),
-    ).moveUpOnHover;
-  }
-}
-
 class BlogMobileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final articles = context.watch<RssFeed>();
+    final articles = context.watch<List<RssItem>>();
     if (articles == null) return CircularProgressIndicator();
     return MobileViewBuilder(
       backgroundColor: Color.fromRGBO(46, 184, 155, 1),
       titleText: BlogView.title,
       children: [
-        for (var i = 0; i < 2; i++)
+        for (final article in articles)
           BlogCard(
+            article: article,
             isMobile: true,
           )
       ],
@@ -118,11 +77,17 @@ class BlogMobileView extends StatelessWidget {
   }
 }
 
-getArticles() async {
+Future<List<RssItem>> getArticles() async {
   final url =
       'https://cors-anywhere.herokuapp.com/https://medium.com/feed/@obrien.sean.dev';
 
   final response = await http.get(url);
   final parsedResponse = RssFeed.parse(response.body);
-  return parsedResponse;
+  final hasFlutterTag = (RssItem article) {
+    return article.categories.any((category) => category.value == 'flutter');
+  };
+  final flutterArticles =
+      parsedResponse.items.where(hasFlutterTag).take(2).toList();
+
+  return flutterArticles;
 }
